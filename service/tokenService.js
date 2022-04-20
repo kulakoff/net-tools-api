d8fe0a55e3f54b891c073a7dadb01b11a7b8d040
Anton Kulakov <anton.kulakoff@ya.ru>
2022-04-20 14:58:02 +0300

    fix changes

--- service/tokenService.js
+++ service/tokenService.js
@@ -4,10 +4,10 @@ const tokenModel = require("./../models/tokenModel");
 class TokenService {
   generateTokens(payload) {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
-      expiresIn: "15m",
+      expiresIn: "15s",
     });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
-      expiresIn: "7d",
+      expiresIn: "1d",
     });
     return { accessToken, refreshToken };
   }
