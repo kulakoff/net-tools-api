da3489b31ce5506a711554a26add80277c28047c
Anton Kulakov <anton.kulakoff@ya.ru>
2022-06-15 11:16:52 +0300

    add debug comments to auth middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,27 +5,27 @@ const authMiddleware = (req, res, next) => {
   try {
     const authorizationHeader = req.headers.authorization;
     if (!authorizationHeader) {
-      console.log("1");
+      console.log("|DEBUG|Отсутствует заголовок|");
       return next(ApiError.UnauthorizedError());
     }
 
     const accessToken = authorizationHeader.split(" ")[1];
     if (!accessToken) {
-      console.log("2");
+      console.log("|DEBUG|Отсутствует токен|");
       return next(ApiError.UnauthorizedError());
     }
 
     const userData = tokenService.valdateAccessToken(accessToken);
-        if (!userData) {
-      console.log("3");
+    if (!userData) {
+      console.log("|DEBUG|Не корректный токен доступа|");
       return next(ApiError.UnauthorizedError());
     }
-
+    //Добавляем данные пользователя из валидного токена к заапросу
     req.user = userData;
-    console.table(req.user)
+    // console.table(req.user);
     next();
   } catch (error) {
-    console.log("4");
+    console.log("|DEBUG|authMiddleware error| ", error);
     return next(ApiError.UnauthorizedError());
   }
 };
